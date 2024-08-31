1d54a8c53a1115a101b3ada596851e4242181b95
adii-the-billionaire <adiishukla196@gmail.com>
2024-08-31 23:46:02 +0530

    solved

--- arrnested.js
+++ arrnested.js
@@ -10,14 +10,29 @@ var solve = function (arr) {
 				b--;
 				a++;
 			}
+		}
+		for (let j = 0; j < arr[i].length - 1; j++) {
+			let a = 0;
+			let b = arr[i].length - 1;
+			while (a < b) {
+				let temp = arr[i][b];
+				arr[i][b] = arr[i][a];
+				arr[i][a] = temp;
+				b--;
+				a++;
+			}
+		}
+		for (let j = 0; j < arr[i].length; j++) {
 			arr[i][j] = Math.abs(arr[i][j] - 1);
 		}
 	}
-	return console.log(arr);
+	return arr;
 };
 
+// Test the function
 solve([
-	[1, 1, 0],
-	[1, 0, 1],
-	[0, 0, 0],
+	[1, 1, 0, 0],
+	[1, 0, 0, 1],
+	[0, 1, 1, 1],
+	[1, 0, 1, 0],
 ]);
