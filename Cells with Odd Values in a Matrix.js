e146421bbf2e9158d36b5e8259a628c7e937ca48
adii-the-billionaire <adiishukla196@gmail.com>
2024-09-09 12:34:21 +0530

    final

--- Cells with Odd Values in a Matrix.js
+++ Cells with Odd Values in a Matrix.js
@@ -1,35 +1,39 @@
-const rows = 2;
-const columns = 3;
-const matrix = Array.from({ length: rows }, () => new Array(columns).fill(0));
-//console.log(matrix);
-let indices = [
-	[0, 1],
-	[1, 1],
-];
-let count = 0;
+var solve = function (n, m, input) {
+	const rows = m;
+	const columns = n;
+	const matrix = Array.from({ length: rows }, () => new Array(columns).fill(0));
 
-//for row side
-for (let i = 0; i < indices.length; i++) {
-	let a = indices[i][0]; //for row side
-	for (let j = 0; j <= matrix.length; j++) {
-		matrix[a][j] += 1;
-	}
-}
-//for coloumns side
-for (let i = 0; i < indices.length; i++) {
-	let a = indices[i][1];
-	for (let j = 0; j < matrix.length; j++) {
-		matrix[j][a] += 1;
-	}
-}
+	let count = 0;
 
-for (let i = 0; i < matrix.length; i++) {
-	for (let j = 0; j < matrix[i].length; j++) {
-		if (matrix[i][j] % 2 != 0) {
-			count++;
+	//for row side
+	for (let i = 0; i < input.length; i++) {
+		let a = input[i][0]; //for row side
+		for (let j = 0; j < matrix.length; j++) {
+			matrix[a][j] += 1;
 		}
 	}
-}
+	//for coloumns side
+	for (let i = 0; i < input.length; i++) {
+		let a = input[i][1];
+		for (let j = 0; j < matrix.length; j++) {
+			matrix[j][a] += 1;
+		}
+	}
 
-console.log(matrix);
-console.log(count);
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			if (matrix[i][j] % 2 != 0) {
+				count++;
+			}
+		}
+	}
+
+	return count;
+};
+
+console.log(
+	solve(2, 3, [
+		[0, 1],
+		[1, 1],
+	]),
+);
