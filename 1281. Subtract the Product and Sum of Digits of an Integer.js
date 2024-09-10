1104a1f72eaa93749ab58e8846af2c598ab38993
adii-the-billionaire <adiishukla196@gmail.com>
2024-09-10 14:30:36 +0530

    done

---
+++ 1281. Subtract the Product and Sum of Digits of an Integer.js
@@ -0,0 +1,19 @@
+// Input: n = 234
+// Output: 15
+// Explanation:
+// Product of digits = 2 * 3 * 4 = 24
+// Sum of digits = 2 + 3 + 4 = 9
+// Result = 24 - 9 = 15
+var subtractProductAndSum = function (n) {
+	let num = n.toString();
+	let product = 1;
+	let sum = 0;
+	for (let i = 0; i < num.length; i++) {
+		product = product * parseInt(num[i]);
+		sum = sum + parseInt(num[i]);
+	}
+	console.log(sum);
+	//console.log(product);
+	return product - sum;
+};
+subtractProductAndSum(234);
