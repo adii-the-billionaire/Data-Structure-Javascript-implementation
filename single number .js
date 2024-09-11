c0f94e236709a0b2dbb1d160609c2c8eeb95f9d0
adii-the-billionaire <adiishukla196@gmail.com>
2024-09-11 16:40:53 +0530

    naive method

--- single number .js
+++ single number .js
@@ -8,17 +8,32 @@
 // Output 2: 4
 //using of hash map
 
-var solve = function(nums) {
-    let map = new Map();
-    let n = nums.length;
+// var solve = function(nums) {
+//     let map = new Map();
+//     let n = nums.length;
 
-    for (let i = 0; i < n; i++) {
-        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
-    }
-    for (let i = 0; i < n; i++) {
-        if (map.get(nums[i]) === 1) {
-            return nums[i];
+//     for (let i = 0; i < n; i++) {
+//         map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+//     }
+//     for (let i = 0; i < n; i++) {
+//         if (map.get(nums[i]) === 1) {
+//             return nums[i];
+//         }
+//     }
+// };
+// console.log(solve([2, 2, 10, 5, 5]));
+var solve = function(nums) {
+    let b = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] == nums[j]) {
+                count = 2;
+            } else {
+                b = nums[i];
+            }
         }
     }
+    return b;
 };
 console.log(solve([2, 2, 10, 5, 5]));
